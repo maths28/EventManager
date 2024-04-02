fa2c4ded5b2b8a0621e6b937b336aee6f5cda63c
Mathieu BOYE <mathieu.boye@web-atrio.COM>
2024-04-02 16:14:22 +0200

    Retour a la page d'accueil en cas d erreur

--- EventManagerUI/src/app/components/error/error.component.ts
+++ EventManagerUI/src/app/components/error/error.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {ErrorService} from "../../service/error.service";
-import {Observable} from "rxjs";
 import {AsyncPipe, DatePipe} from "@angular/common";
 import {MatButton} from "@angular/material/button";
 import {
@@ -11,7 +10,7 @@ import {
   MatCardSubtitle,
   MatCardTitle
 } from "@angular/material/card";
-import {HttpErrorResponse} from "@angular/common/http";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-error',
@@ -25,7 +24,8 @@ import {HttpErrorResponse} from "@angular/common/http";
     MatCardContent,
     MatCardHeader,
     MatCardSubtitle,
-    MatCardTitle
+    MatCardTitle,
+    RouterLink
   ],
   templateUrl: './error.component.html',
   styleUrl: './error.component.css'
@@ -40,9 +40,5 @@ export class ErrorComponent implements OnInit{
 
   ngOnInit() {
     this.errorMessage = this.errorService.errorMessage;
-  }
-
-  onBack(){
-    window.history.go(-2);
   }
 }
